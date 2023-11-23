7e571fc336c17465545356c9d8f20ca0664c703c
dimatereshko95 <dimatereshko95@gmail.com>
2023-11-23 15:42:26 +0300

    Добавьте файлы проекта.

---
+++ WeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WeatherApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetWeather(string city)
+        {
+           
+
+            using (var httpClient = new HttpClient())
+            {
+                var irl = $"{baseUrl}?q={city}&appid={apiKey}&units=metric";
+
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                  
+                }
+
+               
+
+            }
+
+        }
+
+
+    }
+}
---
+++ WeatherApi/Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace WeatherApi.Models
+
+{
+    public class Weather
+    {
+        private string baseUrl = "http://api.openweathermap.org/data/2.5/weather";
+
+        private string ApiKey = "3f0f0b77fac1c7f1786881c23663ad8b";
+    }
+}
